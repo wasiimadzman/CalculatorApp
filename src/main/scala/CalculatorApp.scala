c0cd03a3c964e4c1a33d361aa395252db99902fa
wasiimadzman <thevirtualservices@gmail.com>
2024-07-29 12:45:24 +0800

    calculator stable version 2

--- src/main/scala/CalculatorApp.scala
+++ src/main/scala/CalculatorApp.scala
@@ -15,6 +15,7 @@ object CalculatorApp extends JFXApp {
     alignment = Pos.CenterRight // Align text to the right
     editable = false // Make the display read-only
     text = "" // Initialize with an empty string
+    style = "-fx-background-color: black; -fx-text-fill: white; -fx-font-size: 20px;" // Set display style
   }
 
   // Define the button labels in a grid structure
@@ -24,7 +25,6 @@ object CalculatorApp extends JFXApp {
     Seq("4", "5", "6", "*"),
     Seq("1", "2", "3", "-"),
     Seq(".", "0", "+", "="),
-
   )
 
   // Create a GridPane for the buttons
@@ -36,11 +36,19 @@ object CalculatorApp extends JFXApp {
     // Add buttons to the grid
     buttons.zipWithIndex.foreach { case (row, rowIndex) =>
       row.zipWithIndex.foreach { case (buttonText, colIndex) =>
-        add(new Button(buttonText) {
+        val button = new Button(buttonText) {
           prefWidth = 50 // Set button width
           prefHeight = 50 // Set button height
+          style = if (Seq("/", "*", "-", "+", "=").contains(buttonText)) {
+            "-fx-background-color: blue; -fx-text-fill: white; -fx-font-size: 18px;" // Set operator button style
+          } else if (buttonText == "C") {
+            "-fx-background-color: red; -fx-text-fill: white; -fx-font-size: 18px;" // Set clear button style
+          } else {
+            "-fx-background-color: white; -fx-text-fill: blue; -fx-font-size: 18px;" // Set operand button style
+          }
           onAction = _ => handleInput(buttonText) // Handle button click
-        }, colIndex, rowIndex)
+        }
+        add(button, colIndex, rowIndex)
       }
     }
   }
@@ -73,6 +81,7 @@ object CalculatorApp extends JFXApp {
         alignment = Pos.Center // Center the VBox
         spacing = 20 // Space between elements
         children = Seq(display, buttonGrid) // Add display and button grid to the VBox
+        style = "-fx-background-color: black;" // Set background color of the VBox
       }
     }
   }
